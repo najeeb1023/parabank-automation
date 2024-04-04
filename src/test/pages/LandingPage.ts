6feabf1d34e6fcca4bfb547fc673e1c6467457d9
najeeb1023 <najeeb.capricorn@gmail.com>
2024-04-04 16:32:21 +0200

    Added cucumber console formatter.

--- src/test/pages/LandingPage.ts
+++ src/test/pages/LandingPage.ts
@@ -22,7 +22,7 @@ export class LandingPage {
 
     public async isUserLoggedIn():Promise<void>{
         const assertLogin = await this.landingPageLocators.loginPara();
-        expect(assertLogin).toContainText('Welcome')
+        expect(assertLogin).toContainText('Welcome');
     }
 
     
